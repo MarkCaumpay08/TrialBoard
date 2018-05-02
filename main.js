2d3b1c3f7e69863f5054787d8928dcaa1ce547e5
MarkCaumpay08 <38918024+MarkCaumpay08@users.noreply.github.com>
2018-05-02 23:14:09 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 /* Created by Mark Kevin Mapacpac */
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
